6d0193919932c0b85e2a041a7f8168d76e6f00c2
Emmanuel Okereke <eokereke47@gmail.com>
2022-11-25 00:39:50 +0100

    Edit and Update

--- theblog/forms.py
+++ theblog/forms.py
@@ -13,3 +13,16 @@ class PostForm(forms.ModelForm):
             'body' : forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write Content'}),
 
         }
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'body')
+
+        widgets = {
+            'title' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}),
+            'title_tag' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title Tag'}),
+            'body' : forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write Content'}),
+
+        }
+        
--- theblog/views.py
+++ theblog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, EditForm
 # Create your views here.
 #def home(request):
     #return render(request, 'home.html', {})
@@ -19,6 +19,12 @@ class AddPostView(CreateView):
     model = Post
     form_class = PostForm
     template_name = 'add_post.html'
-    
+
     #fields = '__all__'  # Specify all fields
     #fields = ['title', 'body']
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'update_post.html'
+    # fields = ['title', 'title_tag', 'body']
