46b595f3c21dfa7a4cae351a9f32bd67ee71450b
Emmanuel Okereke <eokereke47@gmail.com>
2022-11-26 12:11:28 +0100

    Hard coding category

--- theblog/forms.py
+++ theblog/forms.py
@@ -1,15 +1,17 @@
 from django import forms
 from .models import Post
 
+choices = [('coding', 'coding'), ('sports', 'sports'), ('entertainment', 'entertainment'),]
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'body')
+        fields = ('title', 'title_tag', 'author', 'category', 'body')
 
         widgets = {
             'title' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}),
             'title_tag' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title Tag'}),
             'author' : forms.Select(attrs={'class': 'form-control', 'placeholder': 'Select Author'}),
+            'category' : forms.Select( choices = choices, attrs={'class': 'form-control', 'placeholder': 'Select Author'}),
             'body' : forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write Content'}),
 
         }
