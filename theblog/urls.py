ecaf75f02e7ced4e12aa77fa6543d33bddad4b86
Emmanuel Okereke <eokereke47@gmail.com>
2022-11-27 22:57:18 +0100

    like blog button

--- theblog/urls.py
+++ theblog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 #from . import views
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView, LikeView
 urlpatterns = [
     #path('', views.home, name='home')
     path('', HomeView.as_view(), name="home"),
@@ -11,5 +11,5 @@ urlpatterns = [
     path('add_category/', AddCategoryView.as_view(), name='add_category'),
     path('category/<str:cats>/', CategoryView, name = 'category'),
     path('category-list', CategoryListView, name = 'category-list'),
-
+    path('like/<int:pk>', LikeView, name='like-post')
 ]
